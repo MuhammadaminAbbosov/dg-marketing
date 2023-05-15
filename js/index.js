6e3a1f9f4fe0d336ec74b594c7c523ec00fbd922
MuhammadaminAbbosov <musulmaanjanob@gmail.com>
2023-05-15 13:28:18 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,378 @@
+const data = {
+  eng: {
+    button: {
+      text1: "Speak to our team today ",
+      text2: "Schedule your free consultation call NOW ",
+    },
+    section1: {
+      title:
+        "We help businesses grow & generate more profit via digital marketing",
+      subTitle: "MODERN ALCHEMY",
+      desc1:
+        "Stop waiting time and money on bad and ineffective marketing agencies",
+      desc2:
+        "It’s time to make your company grow, scale the business to next level and blow up your sales.",
+    },
+    section2: {
+      title: "Our Philosophy",
+      desc1:
+        "Here at Digital Marketing Uz, over 2 years of successful work, we have demonstrated great results to over 70+ best companies in DUBAI and TASHKENT.",
+      desc2:
+        "Our client case studies have become myth, our waiting list is longer than your complaints and we look for a very specific kind of clients.",
+      bold: "Maybe that’s you: it would be our honour to find out. Schedule your FREE call below. ",
+    },
+
+    section3: {
+      subTitle1: "What we offer",
+      title: "Paid Advertisement",
+      services: [
+        "Paid Advertisement ",
+        "Digital Marketing",
+        "WEBSITE",
+        "BRANDING",
+      ],
+      desc1: "Ads, Just Ads.",
+      desc2:
+        "We do one thing - we just do it with a monastic focus and better than anyone else. If you want an agency that offers a full service solution of everything that won’t move the needle forward, we’re not for you.",
+      desc3:
+        "If you want an agency where with two clicks, you can get a clear breakdown of how much was spent, how much was made & what your net profit was - we’re for you.",
+      subTitle2: "MASTERY DEMANDS FOCUS SO....",
+      subTitle3:
+        "We don't offer any other services except for <i>paid advertising...</i>",
+    },
+    section4: {
+      title: "Companies That Trust Us",
+      companies: [
+        {
+          id: 1,
+          title: "FASTECH IT ACADEMY",
+          icon: "https://tashkent.hh.uz/employer-logo/5582529.png",
+        },
+        {
+          id: 2,
+          title: "TOSHKENT TRACTORS",
+          icon: "https://autoline-au.com/img/dealers/logos/b/f/1647516854359911350.png",
+        },
+        {
+          id: 3,
+          title: "FAST AGRO TRAKTORS",
+          icon: "../images/FAST AGRO TRAKTORS.png",
+        },
+        {
+          id: 4,
+          title: "SKAZKA RESTAURANT",
+          icon: "https://www.russianrestaurantdxb.com/gallery/123-ts1623844870.svg",
+        },
+        {
+          id: 5,
+          title: "LITTLE ITALY",
+          icon: "https://cdn.shopify.com/s/files/1/0552/7155/5266/files/Little-Italy-logo_185x@2x.png?v=1631002744",
+        },
+      ],
+    },
+    section5: {
+      title: "Meet The A-Team",
+      desc1:
+        "Founded by Abdulaziz Kadirov who's spent the past 2 years attracting the best talent globally. ",
+      desc2:
+        "With our main goal of helping companies all over the world, to scale their business and generate more revenue. Over 70 successful cases all over the world.",
+      empolyeers: [
+        {
+          id: 1,
+          name: "Abdulaziz Kadirov",
+          position: "FOUNDER",
+          img: "",
+        },
+        {
+          id: 2,
+          name: "Davlatbek Babakalanov",
+          position: "Chief Marketing Officer ",
+          img: "",
+        },
+        {
+          id: 3,
+          name: "Elshod Farkhodov",
+          position: "CONTENT CREATOR",
+          img: "",
+        },
+      ],
+    },
+
+    section6: {
+      title: "Free 15-Minute Demo Call",
+      subTitle1: "SCHEDULE YOUR CALL WITH DANY",
+      desc1:
+        "By the end of this call, you will have a clear understanding of the next steps you can take for your business to start generating consistent and reliable results online with Digital Marketing & Paid Advertising.",
+      desc2:
+        "Find a time on Dava’s calendar to schedule your call today and we look forward to speaking to you soon!",
+      subTitle2: "THIS AUDIT CALL IS PERFECT FOR:",
+      perfects: [
+        "Businesses looking to take their business to next level ",
+        "Businesses looking to take their offline business online ",
+        "Businesses looking to understand their increased revenue potential with funnels & conversion rate optimization.",
+        "Businesses looking to maximize their conversion rates & average order value.",
+        "Businesses looking for a reliable agency that can make their company a priority.",
+      ],
+    },
+  },
+  ru: {
+    button: {
+      text1: "Поговорите с нашей командой сегодня",
+      text2: "Запишитесь на бесплатную консультацию по звонку СЕЙЧАС",
+    },
+    section1: {
+      title:
+        "Мы помогаем бизнесам расти и получать больше прибыли с помощью диджитал маркетинга",
+      subTitle: "СОВРЕМЕННАЯ АЛХИМИЯ",
+      desc1:
+        "Хватит ждать времени и результата от плохих и неэффективных маркетинговых агентств",
+      desc2:
+        "Пришло время сделать так, чтобы ваша компания росла, выходила на новый уровень и увеличивала продажи.",
+    },
+    section2: {
+      title: "Наша философия:",
+      desc1:
+        "В Digital Marketing за 2 года успешной работы мы продемонстрировали отличные результаты более чем 70+ лучшим компаниям в ДУБАЕ и ТАШКЕНТЕ.",
+      desc2:
+        "Наши тематические исследования клиентов стали мифом, наш список ожидания длиннее, чем ваши жалобы, и мы ищем очень специфический тип клиентов.        ",
+      bold: "Может быть, это вы: для нас было бы честью узнать. Запланируйте свой БЕСПЛАТНЫЙ звонок ниже.",
+    },
+
+    section3: {
+      subTitle1: "Что мы предлагаем:",
+      title: "Платная реклама",
+      services: [
+        "Платная реклама",
+        "Диджитал маркетинг",
+        "Web-сайт",
+        "Брендинг",
+      ],
+      desc1: "Реклама, просто реклама.",
+      desc2:
+        "Мы делаем одно — просто делаем это с монашеской направленностью и лучше всех. Если вам нужно агентство, которое предлагает комплексное решение всего, что не продвинет иглу вперед, мы не для вас.",
+      desc3:
+        "Если вам нужно агентство, в котором в два клика вы можете получить четкую разбивку о том, сколько было потрачено, сколько было заработано и какова ваша чистая прибыль - мы для вас.",
+      subTitle2: "МАСТЕРСТВО ТРЕБУЕТ ТАКОЙ СОСРЕДОТОЧЕННОСТИ....",
+      subTitle3:
+        "Мы не предлагаем никаких других услуг, кроме платной рекламы...",
+    },
+    section4: {
+      title: "Компании, которые нам доверяют",
+      companies: [
+        {
+          id: 1,
+          title: "FASTECH IT ACADEMY",
+          icon: "https://tashkent.hh.uz/employer-logo/5582529.png",
+        },
+        {
+          id: 2,
+          title: "TOSHKENT TRACTORS",
+          icon: "https://autoline-au.com/img/dealers/logos/b/f/1647516854359911350.png",
+        },
+        {
+          id: 3,
+          title: "FAST AGRO TRAKTORS",
+          icon: "../images/FAST AGRO TRAKTORS.png",
+        },
+        {
+          id: 4,
+          title: "SKAZKA RESTAURANT",
+          icon: "https://www.russianrestaurantdxb.com/gallery/123-ts1623844870.svg",
+        },
+        {
+          id: 5,
+          title: "LITTLE ITALY",
+          icon: "https://cdn.shopify.com/s/files/1/0552/7155/5266/files/Little-Italy-logo_185x@2x.png?v=1631002744",
+        },
+      ],
+    },
+    section5: {
+      title: "Познакомьтесь с командой:",
+      desc1:
+        "Основана Абдулазизом Кадыровым, который провел последний год, привлекая крутые таланты страны. ",
+      desc2:
+        "Нашей главной целью является помощь компаниям по всему миру в расширении их бизнеса и увеличении доходов. Более 70 успешных кейсов в Дубае и Ташкенте.",
+      empolyeers: [
+        {
+          id: 1,
+          name: "Abdulaziz Kadirov",
+          position: "ОСНОВАТЕЛЬ",
+          img: "https://images.clickfunnels.com/24/1f8e51a5fc4331bf9b03513b9bb882/Iman-Gadzhi.png",
+        },
+        {
+          id: 2,
+          name: "Davlatbek Babakalanov",
+          position: "Директор по маркетингу",
+          img: "https://images.clickfunnels.com/24/1f8e51a5fc4331bf9b03513b9bb882/Iman-Gadzhi.png",
+        },
+        {
+          id: 3,
+          name: "Elshod Farkhodov",
+          position: "Специплист По Контениу в Соц Сетях",
+          img: "https://images.clickfunnels.com/24/1f8e51a5fc4331bf9b03513b9bb882/Iman-Gadzhi.png",
+        },
+      ],
+    },
+
+    section6: {
+      title: "БЕСПЛАТНЫЙ 30-минутный звонок с DAVA (CMO)",
+      subTitle1: "ЗАПЛАНИРУЙТЕ ЗВОНОК С ДЭНИ",
+      desc1:
+        "К концу этого звонка у вас будет четкое представление о следующих шагах, которые вы можете предпринять, чтобы ваш бизнес начал получать стабильные и надежные результаты в Интернете с помощью цифрового маркетинга и платной рекламы.",
+      desc2:
+        "Найдите время в календаре Давы, чтобы запланировать звонок сегодня, и мы с нетерпением ждем встречи с вами в ближайшее время!",
+      subTitle2: "ЭТОТ АУДИТОРСКИЙ ЗВОНОК ИДЕАЛЬНО ДЛЯ:",
+      perfects: [
+        "Компании, стремящиеся вывести свой бизнес на новый уровень",
+        "Компании, желающие перевести свой офлайн-бизнес в онлайн.",
+        "Компании, которые хотят понять свой потенциал увеличения доходов с помощью воронок и оптимизации коэффициента конверсии.",
+        "Компании, стремящиеся максимизировать коэффициент конверсии и среднюю стоимость заказа.",
+        "Бизнес ищет надежное агентство, которое может сделать их компанию приоритетом.",
+      ],
+    },
+  },
+};
+
+dropdown.addEventListener("click", (e) => {
+  e.preventDefault();
+  languagesBox.classList.toggle("active-box");
+});
+
+languages.forEach((lang, index) => {
+  lang.addEventListener("click", () => {
+    languages.forEach((item) => {
+      item.classList.remove("active-lang");
+    });
+    lang.classList.add("active-lang");
+    localStorage.setItem("lang", lang.innerHTML);
+    currentLang.innerHTML = lang.innerHTML;
+
+    if (lang.innerHTML === "🇷🇺 Ru") {
+      changeLang(data.ru);
+    } else {
+      changeLang(data.eng);
+    }
+  });
+});
+
+if (localStorage.getItem("lang")) {
+  currentLang.innerHTML = localStorage.getItem("lang");
+
+  languages.forEach((lang) => {
+    if (lang.innerHTML === localStorage.getItem("lang")) {
+      lang.classList.add("active-lang");
+    } else {
+      lang.classList.remove("active-lang");
+    }
+  });
+
+  if (localStorage.getItem("lang") === "🇷🇺 Ru") {
+    changeLang(data.ru);
+  } else {
+    changeLang(data.eng);
+  }
+}
+
+function changeLang(lang) {
+  // Section-1
+  section1Title.innerHTML = lang.section1.title;
+  section1SubTitle.innerHTML = lang.section1.subTitle;
+  section1Desc1.innerHTML = lang.section1.desc1;
+  section1Desc2.innerHTML = lang.section1.desc2;
+
+  sectionBtnText1.forEach((btn) => {
+    btn.innerHTML = lang.button.text1;
+  });
+  sectionBtnText2.forEach((btn) => {
+    btn.innerHTML = lang.button.text2;
+  });
+
+  // Section-2
+  section2Title.innerHTML = lang.section2.title;
+  section2Desc1.innerHTML = lang.section2.desc1;
+  section2Desc2.innerHTML = lang.section2.desc2;
+  section2Bold.innerHTML = lang.section2.bold;
+
+  // Section-3
+  section3Title.innerHTML = lang.section3.title;
+  section3Desc1.innerHTML = lang.section3.desc1;
+  section3Desc2.innerHTML = lang.section3.desc2;
+  section3Desc3.innerHTML = lang.section3.desc3;
+
+  section3SubTitle1.innerHTML = lang.section3.subTitle1;
+  section3SubTitle2.innerHTML = lang.section3.subTitle2;
+  section3SubTitle3.innerHTML = lang.section3.subTitle3;
+
+  section3Services.innerHTML = "";
+  lang.section3.services.forEach((item) => {
+    const serviceItem = document.createElement("li");
+    const serviceBold = document.createElement("b");
+    const serviceIcon = document.createElement("i");
+
+    serviceIcon.setAttribute("class", "fa-fw far fa-times-circle");
+    serviceIcon.style.color = "rgb(255, 21, 0)";
+    serviceBold.innerHTML = item;
+    serviceItem.append(serviceIcon, serviceBold);
+    section3Services.appendChild(serviceItem);
+  });
+
+  // Section-4
+  section4Title.innerHTML = lang.section4.title;
+
+  lang.section4.companies.forEach((item) => {
+    const box = document.createElement("div")
+    const img = document.createElement("img");
+
+    box.classList.add("company");
+    img.src = item.icon;
+    box.appendChild(img)
+
+    section4Companies.appendChild(box);
+  });
+
+  // Section-5
+
+  section5Title.innerHTML = lang.section5.title
+  section5Desc1.innerHTML = lang.section5.desc1
+  section5Desc2.innerHTML = lang.section5.desc2
+
+  lang.section5.empolyeers.forEach(item => {
+    const card = document.createElement("div")
+    const cardImg = document.createElement("img")
+    const cardName = document.createElement("b")
+    const cardDesc = document.createElement("p")
+
+    card.classList.add("card")
+
+    cardImg.src = item.img
+    cardName.innerHTML = item.name
+    cardDesc.innerHTML = item.position
+    
+
+    card.append(cardImg, cardName, cardDesc)
+
+    section5Employers.appendChild(card)
+  })
+
+  // Section-6
+  section6Title.innerHTML = lang.section6.title;
+  section6Desc1.innerHTML = lang.section6.desc1;
+  section6Desc2.innerHTML = lang.section6.desc2;
+
+  section6SubTitle1.innerHTML = lang.section6.subTitle1;
+  section6SubTitle2.innerHTML = lang.section6.subTitle2;
+  section6Perfects.forEach(perfects => {
+    perfects.innerHTML =""
+    lang.section6.perfects.forEach(item => {
+      const perfectsItem = document.createElement("li");
+      const perfectsText = document.createElement("p");
+      const perfectsIcon = document.createElement("i");
+  
+      perfectsIcon.setAttribute("class", "fa-fw fas fa-check-circle");
+      perfectsIcon.style.color = "rgb(252, 55, 146)";
+      perfectsText.innerHTML = item;
+      perfectsItem.append(perfectsIcon, perfectsText);
+      perfects.appendChild(perfectsItem);
+    })
+  })
+}
---
+++ js/variables.js
@@ -0,0 +1,55 @@
+const dropdown = document.querySelector(".dropdown");
+const languages = Array.from(dropdown.querySelectorAll(".lang"));
+const languagesBox = dropdown.querySelector(".languages");
+const currentLang = document.querySelector(".current");
+// Button
+const sectionBtnText1 = Array.from(
+  document.querySelectorAll(".section-btn__text1")
+);
+const sectionBtnText2 = Array.from(
+  document.querySelectorAll(".section-btn__text2")
+);
+
+// Section-1
+const section1Title = document.querySelector(".section-1__title");
+const section1SubTitle = document.querySelector(".section-1__subTitle");
+const section1Desc1 = document.querySelector(".section-1__desc1");
+const section1Desc2 = document.querySelector(".section-1__desc2");
+
+// Section-2
+const section2Title = document.querySelector(".section-2__title");
+const section2Desc1 = document.querySelector(".section-2__desc1");
+const section2Desc2 = document.querySelector(".section-2__desc2");
+const section2Bold = document.querySelector(".section-2__bold");
+
+// Section-3
+const section3Title = document.querySelector(".section-3__title");
+const section3SubTitle1 = document.querySelector(".section-3__subTitle1");
+const section3SubTitle2 = document.querySelector(".section-3__subTitle2");
+const section3SubTitle3 = document.querySelector(".section-3__subTitle3");
+const section3Desc1 = document.querySelector(".section-3__desc1");
+const section3Desc2 = document.querySelector(".section-3__desc2");
+const section3Desc3 = document.querySelector(".section-3__desc3");
+const section3Services = document.querySelector(".services");
+
+// Section-4
+const section4Title = document.querySelector(".section-4__title");
+const section4Companies= document.querySelector(".companies");
+
+
+// Section-5
+
+const section5Title = document.querySelector(".section-5__title")
+const section5Desc1 = document.querySelector(".section-5__desc1");
+const section5Desc2 = document.querySelector(".section-5__desc2");
+const section5Employers = document.querySelector(".section-5__employers");
+
+// Section-6
+
+const section6Title = document.querySelector(".section-6__title")
+const section6Img = document.querySelector(".section-6__img")
+const section6Desc1 = document.querySelector(".section-6__desc1");
+const section6Desc2 = document.querySelector(".section-6__desc2");
+const section6SubTitle1 = document.querySelector(".section-6__subTitle1");
+const section6SubTitle2 = document.querySelector(".section-6__subTitle2");
+const section6Perfects = Array.from(document.querySelectorAll(".perfects"))
